5ce127af5f4522a9cecc8ef8bd3970dfa16ee5a3
lckdscl <lckdscl+github@protonmail.com>
2022-12-16 17:05:46 +0000

    day 13

--- day13.py
+++ day13.py
@@ -1,44 +1,49 @@
 #!/usr/bin/env python
-"""Day 12"""
+"""Day 13"""
 
-import numpy as np
 import json
 import tool
 
 
-def main():
-    def is_in_order(lefty, righty):
-        for idx in range(max(len(lefty), len(righty))):
-            if idx > len(lefty) - 1:
+def is_in_order(lefty, righty):
+    for idx in range(max(len(lefty), len(righty))):
+        if idx > len(lefty) - 1:
+            return True
+        if idx > len(righty) - 1:
+            return False
+        left_element, right_element = lefty[idx], righty[idx]
+        if isinstance(left_element, int) and isinstance(right_element, int):
+            if left_element < right_element:
                 return True
-            if idx > len(righty) - 1:
+            if right_element < left_element:
                 return False
-            left_element, right_element = lefty[idx], righty[idx]
-            if isinstance(left_element, int) and isinstance(right_element, int):
-                if left_element < right_element:
-                    return True
-                elif right_element < left_element:
-                    return False
-                else:
-                    continue
-            if isinstance(left_element, int):
-                left_element = [left_element]
-            if isinstance(right_element, int):
-                right_element = [right_element]
-            answ = is_in_order(left_element, right_element)
-            if answ is None:
-                continue
-            return answ
+            continue
+        if isinstance(left_element, int):
+            left_element = [left_element]
+        if isinstance(right_element, int):
+            right_element = [right_element]
+        answ = is_in_order(left_element, right_element)
+        if answ is None:
+            continue
+        return answ
 
+
+def main():
     data = list(filter(None, [line.strip("\n") for line in tool.read_return_data(13)]))
     tally = 0
     for idx, item in enumerate(data[::2]):
         left, right = json.loads(item), json.loads(data[1::2][idx])
-        if is_in_order(left, right):
-            tally += idx + 1
-
+        tally += (idx + 1) * (is_in_order(left, right) is True)
     print("Part 1:", tally)
-    print("Part 2:")
+    print("Part 2:", check_list([[2]], data) * (check_list([[6]], data) + 1))
+
+
+def check_list(compared, data):
+    rank = 0
+    for _, item in enumerate(data):
+        left, right = json.loads(item), compared
+        rank += is_in_order(left, right) is True
+    return rank + 1
 
 
 if __name__ == "__main__":
