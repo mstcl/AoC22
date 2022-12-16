459062d29e2ddb878d46d39cf090b9f9cb1a8fae
lckdscl <lckdscl+github@protonmail.com>
2022-12-16 17:05:38 +0000

    add more

--- tool.py
+++ tool.py
@@ -3,4 +3,8 @@
 
 def read_return_data(day: int) -> list:
     with open(f"day{day}-input", "r", encoding="utf-8") as file:
-        return "*".join(file.readlines()).strip("\n").split("*")
+        return "$".join(file.readlines()).strip("\n").split("$")
+
+def read_return_data_2(day:int):
+    with open(f"day{day}-input", "r", encoding="utf-8") as file:
+        return file.read()
